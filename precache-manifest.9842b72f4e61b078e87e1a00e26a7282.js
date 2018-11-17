91908e11bc73d8dfa66b83509d4ee652e40ff66a
Max Efimov <hapsidra@gmail.com>
2018-11-18 02:01:46 +0900

    Updates

---
+++ precache-manifest.9842b72f4e61b078e87e1a00e26a7282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a1eebc9b5c85bbcb8a",
+    "url": "/react-snake/static/js/runtime~main.d1a1eebc.js"
+  },
+  {
+    "revision": "fb7680978127fa4840f5",
+    "url": "/react-snake/static/js/main.fb768097.chunk.js"
+  },
+  {
+    "revision": "10d2f0e9aa40d2dd6d1d",
+    "url": "/react-snake/static/js/1.10d2f0e9.chunk.js"
+  },
+  {
+    "revision": "fb7680978127fa4840f5",
+    "url": "/react-snake/static/css/main.49790b9f.chunk.css"
+  },
+  {
+    "revision": "9a3595eb4599efcd77a3c9bde253f511",
+    "url": "/react-snake/index.html"
+  }
+];
